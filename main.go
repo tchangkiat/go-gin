fb8c45505f196e4805ca1f732780ff1e897d9faa
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-06-18 14:16:42 +0000

    Re-enabled AWS X-Ray tracing feature

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	//"context"
+	"context"
 	"log"
 	"net/http"
 
-	//"os"
+	"os"
 
 	"go-gin/routes"
 
-	//"github.com/aws/aws-xray-sdk-go/v2/awsplugins/ec2"
-	//"github.com/aws/aws-xray-sdk-go/v2/xray"
+	"github.com/aws/aws-xray-sdk-go/v2/awsplugins/ec2"
+	"github.com/aws/aws-xray-sdk-go/v2/xray"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,21 +20,19 @@ func main() {
 	// AWS X-Ray
 	// -----------------------------
 
-	// if os.Getenv("TRACING") == "true" {
-	// 	// conditionally load plugin
-	// 	if os.Getenv("GIN_MODE") == "release" {
-	// 		ec2.Init()
-	// 	}
+	// conditionally load plugin
+	if os.Getenv("GIN_MODE") == "release" {
+		ec2.Init()
+	}
 
-	// 	xrayDaemonAddr := "127.0.0.1:2000"
-	// 	if os.Getenv("AWS_XRAY_DAEMON_ADDRESS") != "" {
-	// 		xrayDaemonAddr = os.Getenv("AWS_XRAY_DAEMON_ADDRESS")
-	// 	}
-	// 	xray.Configure(xray.Config{
-	// 		DaemonAddr:     xrayDaemonAddr,
-	// 		ServiceVersion: "1.2.3",
-	// 	})
-	// }
+	xrayDaemonAddr := "127.0.0.1:2000"
+	if os.Getenv("AWS_XRAY_DAEMON_ADDRESS") != "" {
+		xrayDaemonAddr = os.Getenv("AWS_XRAY_DAEMON_ADDRESS")
+	}
+	xray.Configure(xray.Config{
+		DaemonAddr:     xrayDaemonAddr,
+		ServiceVersion: "1.2.3",
+	})
 
 	// -----------------------------
 
@@ -46,16 +45,11 @@ func main() {
 }
 
 func handleTracingAndError(c *gin.Context) {
-	// if os.Getenv("TRACING") == "true" {
-	// 	// Create a segment for tracing in AWS X-Ray
-	// 	_, seg := xray.BeginSegment(context.Background(), "go-gin")
-	// 	c.Next()
-	// 	// Close the segment after processing the request
-	// 	seg.Close(nil)
-	// } else {
-	// 	c.Next()
-	// }
+	// Create a segment for tracing in AWS X-Ray
+	_, seg := xray.BeginSegment(context.Background(), "go-gin")
 	c.Next()
+	// Close the segment after processing the request
+	seg.Close(nil)
 
 	for _, err := range c.Errors {
 		log.Println(err)
